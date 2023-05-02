54dc87e5eb0d893a893375874256962513ed5d72
Roshan Subedi <rsubedi1@unm.edu>
2023-05-02 17:33:55 -0600

    Agent GUI

---
+++ src/AgentGUI.java
@@ -0,0 +1,10 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AgentGUI extends Application {
+    private static Agent agent;
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
