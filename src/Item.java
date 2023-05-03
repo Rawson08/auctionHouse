4f8450fc08607df287b13fe8ef92d3f7c3fe854c
Atah Omar <aomar1@unm.edu>
2023-05-02 18:30:03 -0600

    Added Items for auction

--- src/Item.java
+++ src/Item.java
@@ -5,7 +5,23 @@
  * @param startingPrice is the minimum bid amount to start
  */
 public record Item(String name, String description, double startingPrice) {
-//TODO: determine how these items will be generated
+
+    public static final Item iPhone = new Item("IPhone","iPhone 12",800);
+    public static final Item ps5 = new Item("PS5","Play Station 5",500);
+    public static final Item xbox = new Item("XBX","Xbox Series X",500);
+
+    public static Item getIPhone() {
+        return iPhone;
+    }
+
+    public static Item getPs5() {
+        return ps5;
+    }
+
+    public static Item getXbox() {
+        return xbox;
+    }
+
     /**
      * @return String with name, description, and starting price
      */
